dbfb349239e6ab438876dd778876ac814f7f3c2a
rochdi <rochdi.abdessamad49@gmail.com>
2023-09-03 18:11:42 +0100

    update seo and logo

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -1,108 +1,40 @@
 <script setup>
+import Home from './views/HomeView.vue'
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <!-- <header>
-    <div class="container mx-auto px-4 bg-gray-200">
-      <nav class="flex">
-        <RouterLink class="font-medium text-lg px-2 dark:text-black" to="/">Home</RouterLink>
-        <RouterLink class="font-medium text-lg px-2 dark:text-black" to="/about">About</RouterLink>
-      </nav>
-    </div>
-    
-  </header> -->
+  <div class="mx-auto px-5 bg-blue-500">
+     <header class="container  text-gray-400 bg-blue-500 body-font">
+              <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                <a class="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+                 <img src="./assets/image/logo.png" alt="logo">
+                  <span class="ml-3 text-xl">ERROCHDI</span>
+                </a>
+                <nav
+                  class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
+                  <router-link class="mr-5 hover:text-white text-white" to="/">Home</router-link>
+                  <router-link class="mr-5 hover:text-white text-white" to="/about">About</router-link>
+                  <router-link class="mr-5 hover:text-white text-white" to="/">Service</router-link>
+                  <router-link class="mr-5 hover:text-white text-white" to="/about">Contact</router-link>
+                  </nav>
+                <button
+                  class="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">Button
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    class="w-4 h-4 ml-1" viewBox="0 0 24 24">
+                    <path d="M5 12h14M12 5l7 7-7 7"></path>
+                  </svg>
+                </button>
+              </div>
+            </header>
 
-  <header class="text-gray-400 bg-gray-900 body-font">
-  <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-    <a class="flex title-font font-medium items-center text-white mb-4 md:mb-0">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-blue-500 rounded-full" viewBox="0 0 24 24">
-        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-      </svg>
-      <span class="ml-3 text-xl">Tailblocks</span>
-    </a>
-    <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-      <router-link class="mr-5 hover:text-white" to="/">Home</router-link>
-      <router-link class="mr-5 hover:text-white" to="/about">About</router-link>
-      <a class="mr-5 hover:text-white">Services</a>
-      <a class="mr-5 hover:text-white">Contact</a>
-    </nav>
-    <button class="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">Button
-      <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
-        <path d="M5 12h14M12 5l7 7-7 7"></path>
-      </svg>
-    </button>
-  </div>
-</header>
+  </div> 
+    <router-view />
 
-
-
-  <RouterView />
-    
 </template>
-<!-- 
+
 <style scoped>
-/* header {
-  line-height: 1.5;
-  max-height: 100vh;
-} */
 
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style> -->
+</style> 
